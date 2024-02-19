6cf82e4573d66d5bfd84d48c8208122b31a83636
shaimaa-git <shaimaasaidmo@gmail.com>
2024-02-19 08:40:44 +0400

    repete two string in this order abba

---
+++ MakeAbba.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+                // Test cases
+                System.out.println(makeAbba("Hi", "Shaima"));
+                System.out.println(makeAbba("my", "car"));
+                System.out.println(makeAbba("What's", "Up"));
+            }
+
+            public static String makeAbba(String a, String b) {
+                return a + b + b + a;
+            }
+        }
+
+    
+
+
