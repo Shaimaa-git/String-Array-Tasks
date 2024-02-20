60fa679ee00d06cfef9ef1e9d05a053e3265c775
shaimaa-git <shaimaasaidmo@gmail.com>
2024-02-20 18:19:01 +0400

    remove the ends of string even if the length was less then 2

---
+++ WithoutEnd2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class WithouEnd2 {
+    public static void main(String[] args) {
+        // Create a Scanner object to take input from the user
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter a string
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();
+
+        // Call the withouEnd2 method and print the result
+        System.out.println("Result: " + withouEnd2(inputString));
+
+        // Close the Scanner
+        scanner.close();
+    }
+
+    public static String withouEnd2(String str) {
+        // Check if the length of the string is at least 2
+        if (str.length() >= 2) {
+            // Return the substring excluding the first and last character
+            return str.substring(1, str.length() - 1);
+        }
+
+        // If the length is less than 2 or the string is empty, return an empty string
+        return "";
+    }
+}
