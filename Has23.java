e50df70a912e2ec988344d4c37bc1eed915fa8ca
shaimaa-git <shaimaasaidmo@gmail.com>
2024-02-20 09:15:36 +0400

    check if the array has 2 or 3

---
+++ Has23.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array1 = {4, 5};
+        int[] array2 = {4, 2};
+        int[] array3 = {3, 5};
+
+        System.out.println(has23(array1)); 
+        System.out.println(has23(array2)); 
+        System.out.println(has23(array3)); 
+    }
+
+    public static boolean has23(int[] nums) {
+        // Check if the array contains a 2 or a 3
+        return nums[0] == 2 || nums[0] == 3 || nums[1] == 2 || nums[1] == 3;
+    }
+}
